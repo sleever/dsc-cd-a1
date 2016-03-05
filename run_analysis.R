588f3c44355c1aae1fac38fb3f5827db5a56224c
Stefan Leever <stefan@bcsa.org>
2016-03-05 19:52:19 +0100

    implemented all necessary application and dataset files

---
+++ run_analysis.R
@@ -0,0 +1,54 @@
+require(dplyr)
+
+createTidyDataset <- function() {
+    getAndMergeData <- function(dataset) {
+    
+        train_subject <- read.table(paste("data/", dataset, "/subject_", dataset, ".txt", sep = ""), header = FALSE, col.names = c("subjectId"))
+        train_subject <- mutate(train_subject, rowNumber = row_number())
+        
+        train_activities <- read.csv(paste("data/", dataset, "/y_", dataset, ".txt", sep = ""), header = FALSE, col.names = c("activityLabelId"))
+        train_activities <- mutate(train_activities, rowNumber = row_number())
+        
+        metaMerge <- merge(train_subject, train_activities, by.x = "rowNumber", by.y = "rowNumber", all = TRUE)
+        
+        features <- read.csv("data/features.txt", header = FALSE, sep = " ", col.names = c("columnId", "columnName"))
+        
+        train_data <- read.table(paste("data/", dataset, "/X_", dataset, ".txt", sep = ""), header = FALSE, col.names = features$columnName)
+        train_data <- mutate(train_data, rowNumber = row_number())
+        
+        merged <- merge(metaMerge, train_data, by.x = "rowNumber", by.y = "rowNumber", all = TRUE)
+        
+        return(merged)
+    
+    }
+    
+    joinDatasets <- function() {
+        testDataset <- getAndMergeData("test")
+        trainDataset <- getAndMergeData("train")
+        newDataset <- rbind(testDataset, trainDataset)
+        return(newDataset)
+    }
+    
+    defineActivities <- function(data) {
+        activity_labels <- read.table("data/activity_labels.txt", header = FALSE, col.names = c("activityId", "activityLabel"))
+        merged <- merge(data, activity_labels, by.x = "activityLabelId", by.y = "activityId", all = FALSE)
+        
+        return(merged)
+    }
+    
+    filterColumns <- function(data) {
+        data <- data[, grep("std|mean|activityLabel|subjectId", colnames(data))]
+        data <- data[, -grep("Freq|activityLabelId", colnames(data))]
+        return(data)
+    }
+    
+    data <- joinDatasets()
+    data <- defineActivities(data)
+    data <- filterColumns(data)
+    
+    return(data)
+}
+
+groupBySubjectIdAndActivity <- function(data) {
+    return(data %>% group_by(subjectId, activityLabel) %>% summarise_each(funs(mean)))
+}
